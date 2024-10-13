513c584c75bcccbc6c8670ca267a6978b22ee29d
Greg <gm226600@falmouth.ac.uk>
2024-10-13 20:08:03 +0100

    Initial commit, setting up file structure and key files.

---
+++ main.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import os
+import discord
+from discord.ext import commands
+import requests
+
+# Load environment variables from the .env file
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+SPOONACULAR_API = os.getenv('SPOONACULAR_API')
+
+# Create the bot object
+bot = commands.Bot(command_prefix=':', intents=discord.Intents.default())
+
+# Handling invalid commands
+@bot.event
+async def on_command_error(ctx, error):
+    # Check if the error is because of an unknown command
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send(f"Oops! I don't recognise that command. Type ':help' for more information.")
+    else:
+    # Handling any other errors
+        await ctx.send("Something went wrong.")
+
+# Help command
+@bot.command(name='help')
+async def custom_help(ctx):
+    help_message = (
+
+    )
+    await ctx.send(help_message)
+
+# Bot Commands here
+
+
+bot.run(os.getenv('DISCORD_TOKEN'))
